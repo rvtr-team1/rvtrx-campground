8a6fa910a10df4fa1c026e868332082a1501cc0e
fred belotte <fredbelotte@outlook.com>
2020-04-07 22:37:58 -0500

    add include module with header, footer components [#172168291]

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { IncludeModule } from './include/include.module';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule],
+  imports: [AppRoutingModule, BrowserModule, IncludeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
