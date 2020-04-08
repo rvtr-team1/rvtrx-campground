032354a3514684ba6eae060e202c003f19283867
fred belotte <fredbelotte@outlook.com>
2020-04-08 00:31:50 -0500

    add unit testing support with puppeteer, junit libraries [#172168291]

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -1,32 +1,36 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+process.env.CHROMIUM_BIN = process.env.CHROMIUM_BIN || require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
+      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-jasmine'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false,
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/rvtr-app-campsite'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require('path').join(__dirname, './test_coverage/rvtr-app-campsite'),
+      reports: ['cobertura', 'lcovonly'],
+      fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: 'test_result/rvtr-app-campsite',
+      outputFile: 'junit-result.xml',
+    },
+    reporters: ['junit', 'progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ['ChromiumHeadless'],
+    singleRun: true,
+    restartOnFileChange: true,
   });
 };
