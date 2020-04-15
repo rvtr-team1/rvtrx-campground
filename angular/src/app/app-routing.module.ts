28c82f0c5d8acbe2ed2b020fbf4c56ddbfbe25a9
fred belotte <fredbelotte@outlook.com>
2020-04-15 18:22:37 -0500

    add account, booking, lodging modules [#172282174]

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,9 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./modules/home/home.module').then((module) => module.HomeModule)
-  }
+    path: 'account',
+    loadChildren: () =>
+      import('./modules/account/account.module').then((module) => module.AccountModule),
+  },
+  {
+    path: 'booking',
+    loadChildren: () =>
+      import('./modules/booking/booking.module').then((module) => module.BookingModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/home/home.module').then((module) => module.HomeModule),
+  },
+  {
+    path: 'lodging',
+    loadChildren: () =>
+      import('./modules/lodging/lodging.module').then((module) => module.LodgingModule),
+  },
 ];
 
 @NgModule({
