ce808fab7eae6c6942f1b08111fad5758f8f7daf
fred belotte <fredbelotte@outlook.com>
2020-04-22 11:50:17 -0500

    add booking http service [#172282272]

--- angular/src/app/services/booking/booking.service.ts
+++ angular/src/app/services/booking/booking.service.ts
@@ -1,9 +1,62 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
+import { ConfigService } from '../config/config.service';
+import { Booking } from '../../data/booking.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookingService {
+  private readonly apiUrl$: Observable<string>;
 
-  constructor() { }
+  /**
+   * Represents the _Booking Service_ `constructor` method
+   *
+   * @param config ConfigService
+   * @param http HttpClient
+   */
+  constructor(private readonly config: ConfigService, private readonly http: HttpClient) {
+    this.apiUrl$ = config.get().pipe(map((cfg) => cfg.api.booking));
+  }
+
+  /**
+   * Represents the _Booking Service_ `delete` method
+   *
+   * @param id string
+   */
+  delete(id: string): Observable<boolean> {
+    return this.apiUrl$.pipe(
+      concatMap((url) => this.http.delete<boolean>(url, { params: { id } }))
+    );
+  }
+
+  /**
+   * Represents the _Booking Service_ `get` method
+   *
+   * @param id string
+   */
+  get(id?: string): Observable<Booking[]> {
+    const options = id ? { params: new HttpParams().set('id', id) } : {};
+    return this.apiUrl$.pipe(concatMap((url) => this.http.get<Booking[]>(url, options)));
+  }
+
+  /**
+   * Represents the _Booking Service_ `post` method
+   *
+   * @param booking Booking
+   */
+  post(booking: Booking): Observable<boolean> {
+    return this.apiUrl$.pipe(concatMap((url) => this.http.post<boolean>(url, booking)));
+  }
+
+  /**
+   * Represents the _Booking Service_ `put` method
+   *
+   * @param booking Booking
+   */
+  put(booking: Booking): Observable<Booking> {
+    return this.apiUrl$.pipe(concatMap((url) => this.http.put<Booking>(url, booking)));
+  }
 }
