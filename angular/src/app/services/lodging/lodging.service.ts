7e3c8f5c08d42e194769da52c1aba644e99e49da
fred belotte <fredbelotte@outlook.com>
2020-04-22 21:35:36 -0500

    add lodging service with http support [#172282285]

--- angular/src/app/services/lodging/lodging.service.ts
+++ angular/src/app/services/lodging/lodging.service.ts
@@ -1,9 +1,62 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { concatMap, map } from 'rxjs/operators';
+import { ConfigService } from '../config/config.service';
+import { Lodging } from '../../data/lodging.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LodgingService {
+  private readonly apiUrl$: Observable<string>;
 
-  constructor() { }
+  /**
+   * Represents the _Lodging Service_ `constructor` method
+   *
+   * @param config ConfigService
+   * @param http HttpClient
+   */
+  constructor(private readonly config: ConfigService, private readonly http: HttpClient) {
+    this.apiUrl$ = config.get().pipe(map((cfg) => cfg.api.lodging));
+  }
+
+  /**
+   * Represents the _Lodging Service_ `delete` method
+   *
+   * @param id string
+   */
+  delete(id: string): Observable<boolean> {
+    return this.apiUrl$.pipe(
+      concatMap((url) => this.http.delete<boolean>(url, { params: { id } }))
+    );
+  }
+
+  /**
+   * Represents the _Lodging Service_ `get` method
+   *
+   * @param id string
+   */
+  get(id?: string): Observable<Lodging[]> {
+    const options = id ? { params: new HttpParams().set('id', id) } : {};
+    return this.apiUrl$.pipe(concatMap((url) => this.http.get<Lodging[]>(url, options)));
+  }
+
+  /**
+   * Represents the _Lodging Service_ `post` method
+   *
+   * @param lodging Lodging
+   */
+  post(lodging: Lodging): Observable<boolean> {
+    return this.apiUrl$.pipe(concatMap((url) => this.http.post<boolean>(url, lodging)));
+  }
+
+  /**
+   * Represents the _Lodging Service_ `put` method
+   *
+   * @param lodging Lodging
+   */
+  put(lodging: Lodging): Observable<Lodging> {
+    return this.apiUrl$.pipe(concatMap((url) => this.http.put<Lodging>(url, lodging)));
+  }
 }
