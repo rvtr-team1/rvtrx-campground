33cacd4bdbd4da100f5bcc6768d4c0a779c3526b
fred belotte <fredbelotte@outlook.com>
2020-04-24 12:56:37 -0500

    add footer navigation support [#172280967]

--- angular/src/app/data/config.model.ts
+++ angular/src/app/data/config.model.ts
@@ -15,6 +15,7 @@ export interface Config {
     lodging: string;
   };
   navigation: {
+    footer: Link[];
     header: Link[];
   };
 }
--- angular/src/app/layout/footer/footer.component.ts
+++ angular/src/app/layout/footer/footer.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Link } from '../../data/link.model';
+import { ConfigService } from '../../services/config/config.service';
+import { map } from 'rxjs/operators';
+import { Config } from 'src/app/data/config.model';
 
 @Component({
   selector: 'uic-footer',
   templateUrl: './footer.component.html',
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  navbarLinks$: Observable<Link[]>;
 
-  ngOnInit(): void {}
+  constructor(private readonly config: ConfigService) {}
+
+  ngOnInit(): void {
+    this.navbarLinks$ = this.config.get().pipe(map<Config, Link[]>((cfg) => cfg.navigation.footer));
+  }
 }
