a26a23373a2decf51e07d83463064a32214d4852
fred belotte <fredbelotte@outlook.com>
2020-04-24 12:45:33 -0500

    add footer html content [#172280967]

--- angular/src/app/layout/footer/footer.component.ts
+++ angular/src/app/layout/footer/footer.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'uic-footer',
   templateUrl: './footer.component.html',
-  styleUrls: [],
 })
 export class FooterComponent implements OnInit {
   constructor() {}
