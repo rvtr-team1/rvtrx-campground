65d247030307895b75e60353aa287032e5af90d7
fred belotte <fredbelotte@outlook.com>
2020-04-25 00:07:52 -0500

    fix test configuration error [#172280967]

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       testFilePattern: '.spec.ts',
       useBrowserName: false,
     },
-    reporters: ['junit', 'progress', 'sonarqubeUnit'],
+    reporters: ['progress', 'sonarqubeUnit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
