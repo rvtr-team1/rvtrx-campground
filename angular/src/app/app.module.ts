42a70949e2080916affb1734dbc4bc6cb1e8e1e2
fred belotte <fredbelotte@outlook.com>
2020-05-07 12:19:42 -0500

    apply monitoring service to app module [#172282468]

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LayoutModule } from './layout/layout.module';
+import { MonitorService } from './services/monitor/monitor.service';
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [AppComponent],
   imports: [AppRoutingModule, BrowserModule, LayoutModule],
-  bootstrap: [AppComponent],
+  providers: [{ provide: ErrorHandler, useClass: MonitorService }],
 })
 export class AppModule {}
