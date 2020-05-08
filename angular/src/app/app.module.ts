0bc6b28930f05c0ef1fd84f38a1312ef1e5a5193
fred belotte <fredbelotte@outlook.com>
2020-05-08 16:44:51 -0500

    fix injector error on e2e tests [#172282468]

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Monitoring } from './data/monitoring.model';
 import { LayoutModule } from './layout/layout.module';
 import { MonitoringService } from './services/monitoring/monitoring.service';
 
@@ -9,6 +10,6 @@ import { MonitoringService } from './services/monitoring/monitoring.service';
   bootstrap: [AppComponent],
   declarations: [AppComponent],
   imports: [AppRoutingModule, BrowserModule, LayoutModule],
-  providers: [{ provide: ErrorHandler, useClass: MonitoringService }],
+  providers: [{ provide: ErrorHandler, useClass: MonitoringService }, Monitoring],
 })
 export class AppModule {}
