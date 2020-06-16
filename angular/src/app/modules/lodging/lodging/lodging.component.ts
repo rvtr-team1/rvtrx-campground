8b7a8946635c7ea05cf01437e1b70c4a68beccc5
fred belotte <fredbelotte@outlook.com>
2020-06-15 22:57:48 -0500

    add lodging component [#172282343]

---
+++ angular/src/app/modules/lodging/lodging/lodging.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'uic-lodging',
+  templateUrl: './lodging.component.html',
+})
+export class LodgingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
