97796d6ca58f156fb7ffde289a19724dff819ddc
fred belotte <fredbelotte@outlook.com>
2020-07-28 23:28:10 -0500

    add account observable property [#173236608]

--- angular/src/app/modules/account/account-routing.module.ts
+++ angular/src/app/modules/account/account-routing.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountComponent } from './account/account.component';
@@ -7,6 +8,6 @@ const routes: Routes = [{ component: AccountComponent, path: '' }];
 @NgModule({
   declarations: [AccountComponent],
   exports: [RouterModule],
-  imports: [RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
 export class AccountRoutingModule {}
--- angular/src/app/modules/account/account/account.component.ts
+++ angular/src/app/modules/account/account/account.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Account } from '../../../data/account.model';
+import { AccountService } from '../../../services/account/account.service';
 
 @Component({
   selector: 'uic-account',
   templateUrl: './account.component.html',
 })
 export class AccountComponent implements OnInit {
-  constructor() {}
+  account$: Observable<Account>;
 
-  ngOnInit(): void {}
+  constructor(private readonly accountService: AccountService) {}
+
+  ngOnInit(): void {
+    this.account$ = of({
+      id: '',
+      address: {
+        id: '',
+        city: '',
+        country: '',
+        postalCode: '',
+        stateProvince: '',
+        street: '',
+      },
+      name: '',
+      payments: [],
+      profiles: [],
+    });
+  }
 }
