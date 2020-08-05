b5ad9b3bf8db1891edd01a8207d123b050ca0911
Luis Gomez <luey714@gmail.com>
2020-08-05 13:48:45 -0700

    added form to search bar

--- angular/src/app/modules/booking/booking-routing.module.ts
+++ angular/src/app/modules/booking/booking-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { BookingComponent } from './booking/booking.component';
 
 const routes: Routes = [{ component: BookingComponent, path: '' }];
@@ -8,6 +9,6 @@ const routes: Routes = [{ component: BookingComponent, path: '' }];
 @NgModule({
   declarations: [BookingComponent],
   exports: [RouterModule],
-  imports: [CommonModule,RouterModule.forChild(routes)],
+  imports: [FormsModule,CommonModule,RouterModule.forChild(routes)],
 })
 export class BookingRoutingModule {}
--- angular/src/app/modules/booking/booking/booking.component.ts
+++ angular/src/app/modules/booking/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 import { Rental } from '../../../data/rental.model';
@@ -15,79 +16,9 @@ export class BookingComponent implements OnInit {
 
   rentals$: Observable<Rental[]>;
   locations$: Observable<Location[]>;
-  // bookings$: Observable<Booking[]>;
+  bookings$: Observable<Booking[]>;
 
-  bookings: Booking[] = [
-    {
-      "id": "1",
-      "accountId": "100",
-      "lodgingId": "1",
-      "guests": [
-        {
-          "id": "100",
-          "email": "campero@demo.com",
-          "name": {
-            "id": "100",
-            "family": "demo",
-            "given": "campero"
-          },
-          "phone": "800-123-9876"
-        },
-        {
-          "id": "200",
-          "email": "campera@demo.com",
-          "name": {
-            "id": "200",
-            "family": "demo",
-            "given": "campera"
-          },
-          "phone": "800-123-9876"
-        }
-      ],
-      "rentals": [
-        {
-          "id": "1",
-          "name": "Unit A",
-          "rentalUnit": {
-            "id": "1",
-            "bathrooms": [
-              {
-                "id": "1",
-                "fixture": 1
-              },
-              {
-                "id": "2",
-                "fixture": 2
-              }
-            ],
-            "bedrooms": [
-              {
-                "id": "1",
-                "count": 2,
-                "type": "Queen"
-              },
-              {
-                "id": "2",
-                "count": 2,
-                "type": "Queen"
-              }
-            ],
-            "name": "Unit A",
-            "occupancy": 4,
-            "type": ""
-          }
-        }
-      ],
-      "stay": {
-        "id": "1",
-        "checkIn": new Date ('2019-01-16'),
-        "checkOut": new Date ("2019-01-17"),
-        "dateCreated": new Date ("2019-01-01"),
-        "dateModified": new Date ("2019-01-06")
-      },
-      "status": "Paid"
-    }
-  ]
+  bookings: Booking[];
   
 
   constructor(
@@ -96,13 +27,12 @@ export class BookingComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.bookingService.get().subscribe(
-      data => this.bookings = data);
+    this.bookings$ = this.bookingService.get();
   }
     
   
 
-  // getBookings() {
-    
-  // }
+  onSubmit(form: NgForm) {
+    console.log('Your form data : ', form.value );
+  }
 }
