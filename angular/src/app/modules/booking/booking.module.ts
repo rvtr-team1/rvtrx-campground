23c7a88ba0ce32a1b94e11cce70732bac614418c
Dusten Knull <dakatk97@gmail.com>
2020-08-05 10:19:39 -0500

    Fixed directory structure

--- angular/src/app/modules/booking/booking.module.ts
+++ angular/src/app/modules/booking/booking.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
 import { SearchComponent } from './search/search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, SearchResultsComponent],
   imports: [
     CommonModule,
     BookingRoutingModule
