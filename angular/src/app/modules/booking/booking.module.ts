da53164be9ebaa6a2900e3e77b855a8303eeeb9d
Dusten Knull <dakatk97@gmail.com>
2020-08-05 09:46:13 -0500

    Added search as subcomponent of booking module

--- angular/src/app/modules/booking/booking.module.ts
+++ angular/src/app/modules/booking/booking.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BookingRoutingModule } from './booking-routing.module';
+import { SearchComponent } from './search/search/search.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchComponent],
   imports: [
     CommonModule,
     BookingRoutingModule
