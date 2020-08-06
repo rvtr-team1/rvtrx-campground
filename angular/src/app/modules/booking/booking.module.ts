209ba5ec849192e4b49c65193228fcddb0f94005
Dusten Knull <dakatk97@gmail.com>
2020-08-06 15:44:52 -0500

    Added search bar component

--- angular/src/app/modules/booking/booking.module.ts
+++ angular/src/app/modules/booking/booking.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BookingRoutingModule } from './booking-routing.module';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports: [BookingRoutingModule],
+  declarations: [SearchBarComponent],
 })
 export class BookingModule {}
