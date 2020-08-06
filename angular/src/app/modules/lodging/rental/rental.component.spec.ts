d40f3b3bae16242f673fcc21396b8c75d2d0c64a
Josh Bertrand <joshuabertrand@protonmail.com>
2020-08-06 17:06:27 -0400

    Began writing additional tests

--- angular/src/app/modules/lodging/rental/rental.component.spec.ts
+++ angular/src/app/modules/lodging/rental/rental.component.spec.ts
@@ -53,5 +53,6 @@ describe('RentalComponent', () => {
   it('should get rental', () => {
     expect(component.lodgings[0].rentals).toBeTruthy();
     expect(component.lodgings[0].rentals).toEqual(lodgings[0].rentals);
+    expect(component.lodgings[0].rentals).toBeNull();
   });
 });
