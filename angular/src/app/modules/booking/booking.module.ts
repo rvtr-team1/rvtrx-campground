e2777eb0ae0861c336f763226372b179c53232fd
Dusten Knull <dakatk97@gmail.com>
2020-08-06 15:51:13 -0500

    Updated booking module

--- angular/src/app/modules/booking/booking.module.ts
+++ angular/src/app/modules/booking/booking.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BookingRoutingModule } from './booking-routing.module';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 
 @NgModule({
   imports: [BookingRoutingModule],
-  declarations: [SearchBarComponent],
 })
 export class BookingModule {}
