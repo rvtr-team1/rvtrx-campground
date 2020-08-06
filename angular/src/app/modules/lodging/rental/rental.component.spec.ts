b20b78bbbfe1434ede8a4e0671d349889ada0bbb
Josh Bertrand <joshuabertrand@protonmail.com>
2020-08-05 22:57:04 -0400

    Finished writing tests. NullInjectorError when running tests currently

--- angular/src/app/modules/lodging/rental/rental.component.spec.ts
+++ angular/src/app/modules/lodging/rental/rental.component.spec.ts
@@ -1,53 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { RentalComponent } from './rental.component';
 import { Router } from '@angular/router';
-import { RentalService } from 'src/app/services/lodging/rental/rental.service';
 import { TemplateDefinitionBuilder } from '@angular/compiler/src/render3/view/template';
+import { Rental } from 'src/app/data/rental.model';
 
 describe('RentalComponent', () => {
   let component: RentalComponent;
   let fixture: ComponentFixture<RentalComponent>;
 
+  const rentals: Rental[] = [
+    {
+      id: '1',
+      name: 'testRental',
+      rentalUnit: {
+        id: '6',
+        bedrooms: [
+          {
+            id: '1',
+            count: 4,
+            type: 'Queen Bed',
+          },
+        ],
+        name: 'Family Room',
+        occupancy: 5,
+        type: 'testType',
+      },
+    },
+  ];
+
+  const rentalComponentSpy = jasmine.createSpyObj('RentalComponent', ['GetLength']);
+  //rentalComponentSpy.get.and.returnValue(of(rentals));
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [RentalComponent],
+      providers: [{ provide: RentalComponent, useValue: rentalComponentSpy }],
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(RentalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
 
-describe('Rental component routing', () => {
-  let component: RentalComponent;
-  let fixture: ComponentFixture<RentalComponent>;
-
-  
-
-  beforeEach(async(() => {
-    const rentalServiceSpy = jasmine.createSpyObj('RentalService', ['get']);
-    //getRentalSpy = rentalServiceSpy.get().and.returnValue(of());
-
-    TestBed.configureTestingModule({
-      declarations: [RentalComponent],
-      providers: [{ provide: RentalService, useValue: rentalServiceSpy }],
-    });
-
-    fixture = TestBed.createComponent(RentalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
-
-  //temporary test until the rental component is complete
-  it('should return rental', () => {
-    expect(component).toBeTruthy();
+  it('should get rental', () => {
+    expect(component.rentals).toBeTruthy();
+    expect(component.rentals).toEqual(rentals);
   });
 });
