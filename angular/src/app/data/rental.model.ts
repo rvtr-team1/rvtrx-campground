110cfd8257124fbf34edea95001f564dfe22b3b3
Dusten Knull <dakatk97@gmail.com>
2020-08-07 12:32:34 -0500

    Mappings for date types in booking component

--- angular/src/app/data/rental.model.ts
+++ angular/src/app/data/rental.model.ts
@@ -11,6 +11,12 @@ import { RentalUnit } from './rental-unit.model';
  * price: number;
  * discountedPrice: number;
  * ```
+ * 
+ * status: Rental occupation/checkin/checkout status
+ * price: Normal nightly cost for the rental
+ * discountedPrice: If there is a discount for this rental, 
+ *                  the discounted price will show instead
+ *                  of the normal price
  */
 export interface Rental {
   id: string;
--- angular/src/app/modules/booking/booking/booking.component.ts
+++ angular/src/app/modules/booking/booking/booking.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Lodging } from 'src/app/data/lodging.model';
 import { LodgingService } from 'src/app/services/lodging/lodging.service';
+import { map } from 'rxjs/operators';
+import { Review } from 'src/app/data/review.model';
 
 @Component({
   selector: 'uic-booking',
@@ -16,5 +18,23 @@ export class BookingComponent implements OnInit {
 
   ngOnInit(): void {
     this.lodgings$ = this.lodgingService.get();
+
+    function mapDates(reviews: Review[]): Review[] {
+      return reviews.map(
+        (review) => {
+
+          review.dateCreated = new Date(review.dateCreated);
+          return review;
+        }
+      )
+    }
+
+    this.lodgings$.pipe(
+      map(
+        (lodgings) => lodgings.map(
+          (lodging) => lodging.reviews = mapDates(lodging.reviews)
+        )
+      )
+    )
   }
 }
