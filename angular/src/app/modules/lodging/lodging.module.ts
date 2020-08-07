8f0a23b0ab03c76a90896945014357d0769f9d4f
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-06 19:21:21 -0600

    Updated formatting, put lodging home component from app module to lodging module

--- angular/src/app/modules/lodging/lodging.module.ts
+++ angular/src/app/modules/lodging/lodging.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LodgingRoutingModule } from './lodging-routing.module';
+import { LodgingHomeComponent } from './lodging-home/lodging-home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LodgingHomeComponent],
   imports: [CommonModule, LodgingRoutingModule],
 })
 export class LodgingModule {}
