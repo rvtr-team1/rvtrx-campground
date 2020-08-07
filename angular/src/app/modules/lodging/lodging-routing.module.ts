2bf77038ec0658c2bcf1443086959d09eda2bb20
Asher Williams <asher.williams1998@gmail.com>
2020-08-07 09:06:21 -0500

    modified lodging routing module in attempt to fix an error

--- angular/src/app/modules/lodging/lodging-routing.module.ts
+++ angular/src/app/modules/lodging/lodging-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LodgingComponent } from './lodging/lodging.component';
-import { LodgingService } from 'src/app/services/lodging/lodging.service';
 
 const routes: Routes = [{ component: LodgingComponent, path: '' }];
 
 @NgModule({
-  declarations: [LodgingComponent],
+  declarations: [],
   exports: [RouterModule],
   imports: [RouterModule.forChild(routes)],
 })
