3f087be1924a5d2ff6010f496985b0b8b94d8cf4
Luis Gomez <luey714@gmail.com>
2020-08-07 08:56:37 -0700

    Date search functionality

--- angular/src/app/modules/booking/booking/booking.component.ts
+++ angular/src/app/modules/booking/booking/booking.component.ts
@@ -8,6 +8,7 @@ import { BookingService } from 'src/app/services/booking/booking.service';
 import { LodgingService } from 'src/app/services/lodging/lodging.service';
 import { Lodging } from 'src/app/data/lodging.model';
 import { map } from 'rxjs/operators';
+import { Location } from '../../../data/location.model';
 
 @Component({
   selector: 'uic-booking',
@@ -20,7 +21,6 @@ export class BookingComponent implements OnInit {
   lodgings$: Observable<Lodging[]>;
   locations$: Observable<Location[]>;
 
-  bookings: Booking[];
   searchResults: Lodging[] = [];
   isSearched: Boolean = false;
 
@@ -38,8 +38,14 @@ export class BookingComponent implements OnInit {
 
     let occupancy = form.value.adults + form.value.children;
     // this.searchByOccupancy(occupancy);
+
+    // this.searchByCityAndOccupancy(form.value.location, occupancy);
+
+    let checkIn = form.value["check-in"]
+    let checkOut = form.value["check-out"]
     
-    this.searchByCityAndOccupancy(form.value.location, occupancy);
+
+    this.searchByDate(checkIn, checkOut);
     this.isSearched = true;
   }
   
@@ -75,6 +81,89 @@ export class BookingComponent implements OnInit {
       )
     )
   }
+
+  searchByDate(checkIn: Date, checkOut: Date) {
+    console.log(checkIn, checkOut);
+    this.lodgings$ = this.lodgingService.get();
+    this.searchResults = [];
+
+    let lodgingRentals = [];
+    let bookingRentals = [];
+    let availableRentals = [];
+
+    this.bookings$.subscribe(
+      bookings => bookings.forEach(
+        booking => booking.rentals.forEach(
+          bookingRental => {
+            bookingRentals.push(bookingRental);
+          }
+        )
+      )
+    )
+    
+    console.log(bookingRentals);
+
+    this.lodgings$.subscribe(
+      lodgings => lodgings.forEach(
+        lodging => lodging.rentals.forEach(
+          lodgingRental => {
+            lodgingRentals.push(lodgingRental);
+          }
+        )
+      )
+    )
+    
+    console.log(lodgingRentals);
+          debugger;
+    this.bookings$.subscribe(
+      bookings => bookings.forEach(
+        booking => {
+          console.log(booking);
+          if (checkIn < booking.stay.checkIn && checkOut < booking.stay.checkIn) {
+            lodgingRentals.forEach(
+              lodgingRental => {
+                if (booking.rentals.includes(lodgingRental)) {
+                  console.log(`lodging rental ${lodgingRental.name} is available`)
+                }
+              }
+            )
+          }
+          else if (checkIn > booking.stay.checkOut && checkOut > booking.stay.checkOut ) {
+            lodgingRentals.forEach(
+              lodgingRental => {
+                if (booking.rentals.includes(lodgingRental)) {
+                  console.log(`lodging rental ${lodgingRental.name} is available`)
+                }
+              }
+            )
+          }
+        }
+      )
+    )
+    
+
+    console.log(availableRentals);
+    
+    this.lodgings$.subscribe(
+      lodgings => lodgings.forEach(
+        lodging => {
+           availableRentals.forEach(
+             availableRental => {
+               if (lodging.rentals.includes(availableRental)) {
+                 this.searchResults.push(lodging);
+               }
+             }
+           )
+        }
+      )
+    )
+
+    
+
+    console.log(this.searchResults);
+    this.lodgings$ = of(this.searchResults);
+
+  }
   
   searchByCityAndOccupancy(city: string, occupancy: number){
     console.log(city, occupancy);
@@ -103,6 +192,7 @@ export class BookingComponent implements OnInit {
 
   ngOnInit(): void {
     this.lodgings$ = this.lodgingService.get();
+    this.bookings$ = this.bookingService.get();
 
     this.lodgings$.pipe(
       map((lodgings) => {
@@ -116,10 +206,8 @@ export class BookingComponent implements OnInit {
       })
     );
 
-    this.lodgings$.pipe(
-      map((lodgings) => {
-        lodgings.map((lodging) => lodging.location);
-      })
+    this.lodgings$.subscribe (
+      lodgings => this.locations$ = of(lodgings.map(lodging => lodging.location))
     );
   }
 }
