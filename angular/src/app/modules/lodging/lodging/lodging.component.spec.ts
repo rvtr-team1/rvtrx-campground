8c221bb06f7f345174072bae6b7434b6083becb0
Asher Williams <asher.williams1998@gmail.com>
2020-08-07 15:00:28 -0500

    removed my attempt to fix the testing error in lodging component

--- angular/src/app/modules/lodging/lodging/lodging.component.spec.ts
+++ angular/src/app/modules/lodging/lodging/lodging.component.spec.ts
@@ -2,42 +2,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LodgingComponent } from './lodging.component';
 import { HttpErrorResponse, HttpClientModule } from '@angular/common/http';
 import { RouterModule, ActivatedRoute } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class ActivatedRouteStub {
-  private subject = new BehaviorSubject(this.testParams);
-  params = this.subject.asObservable();
-
-  private _testParams: {};
-  get testParams() {
-    return this._testParams;
-  }
-  set testParams(params: {}) {
-    this._testParams = params;
-    this.subject.next(params);
-  }
-}
 
 describe('LodgingComponent', () => {
-  let mockParams, mockActivatedRoute: any;
   let component: LodgingComponent;
   let fixture: ComponentFixture<LodgingComponent>;
 
   beforeEach(async(() => {
-    mockActivatedRoute = new ActivatedRouteStub();
     TestBed.configureTestingModule({
       declarations: [LodgingComponent],
       imports: [RouterModule.forRoot([]), HttpClientModule],
-      providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LodgingComponent);
     component = fixture.componentInstance;
-    mockActivatedRoute.testParams = { id: '1' };
     fixture.detectChanges();
   });
 
