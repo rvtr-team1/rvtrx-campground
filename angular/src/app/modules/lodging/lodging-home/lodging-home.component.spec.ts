16890380f339f15402cc06494aea2ea96fe4177a
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-06 18:48:36 -0600

    test checking that template display info correctly

--- angular/src/app/modules/lodging/lodging-home/lodging-home.component.spec.ts
+++ angular/src/app/modules/lodging/lodging-home/lodging-home.component.spec.ts
@@ -3,47 +3,44 @@ import { LodgingHomeComponent } from './lodging-home.component';
 import { of } from 'rxjs';
 import { Lodging } from 'src/app/data/lodging.model';
 import { LodgingService } from 'src/app/services/lodging/lodging.service';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('LodgingHomeComponent', () => {
   let component: LodgingHomeComponent;
   let fixture: ComponentFixture<LodgingHomeComponent>;
 
-  const lodgings: Lodging[] =
-    [
-      {
+  const lodgings: Lodging[] = [
+    {
+      id: '1',
+      location: {
         id: '1',
-        location: {
+        address: {
           id: '1',
-          address: {
-            id: '1',
-            city: 'testCity',
-            country: 'testCountry',
-            postalCode: 'testCode',
-            stateProvince: 'testState',
-            street: 'testStreet'
-          },
-          latitude: 'testLat',
-          locale: 'testLocale',
-          longitude: 'testLong'
+          city: 'testCity',
+          country: 'testCountry',
+          postalCode: 'testCode',
+          stateProvince: 'testState',
+          street: 'testStreet',
         },
-        name: 'test',
-        rentals: [],
-        reviews: []
-      }
-    ];
+        latitude: 'testLat',
+        locale: 'testLocale',
+        longitude: 'testLong',
+      },
+      name: 'test',
+      rentals: [],
+      reviews: [],
+    },
+  ];
 
   beforeEach(async(() => {
-
     const lodgingService = jasmine.createSpyObj('LodgingService', ['get']);
     lodgingService.get.and.returnValue(of(lodgings));
 
     TestBed.configureTestingModule({
-      declarations: [ LodgingHomeComponent ],
-      providers: [
-        {provide: LodgingService, useValue: lodgingService}
-      ]
-    })
-    .compileComponents();
+      declarations: [LodgingHomeComponent],
+      providers: [{ provide: LodgingService, useValue: lodgingService }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LodgingHomeComponent);
     component = fixture.componentInstance;
@@ -63,6 +60,17 @@ describe('LodgingHomeComponent', () => {
   it('should get lodgings on initialization', () => {
     expect(component.lodgings).toBeTruthy();
     expect(component.lodgings).toEqual(lodgings);
+  });
+
+  /**
+   * tests if the lodging name and address is displayed in the template
+   */
+  it('should display lodging info in the template', () => {
+    const info = fixture.debugElement.nativeElement.querySelectorAll('p');
+    expect(info[0].textContent).toContain('test');
+    expect(info[1].textContent).toContain('testStreet');
+    expect(info[2].textContent).toContain('testCity, testState, testCountry');
+    expect(info[3].textContent).toContain('testCode');
   });
 
   /**
