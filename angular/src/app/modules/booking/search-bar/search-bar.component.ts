41369c667d8fda8eb691bea7ffd87afdc7933f19
Luis Gomez <luey714@gmail.com>
2020-08-10 12:37:30 -0700

    finished search functionality and refactored code

--- angular/src/app/modules/booking/search-bar/search-bar.component.ts
+++ angular/src/app/modules/booking/search-bar/search-bar.component.ts
@@ -4,8 +4,6 @@ import { Observable, of } from 'rxjs';
 
 import { Location } from '../../../data/location.model';
 
-import { map } from 'rxjs/operators';
-
 import { Rental } from '../../../data/rental.model';
 import { Booking } from '../../../data/booking.model';
 import { BookingService } from 'src/app/services/booking/booking.service';
@@ -32,10 +30,7 @@ export class SearchBarComponent implements OnInit {
   constructor(private bookingService: BookingService, private lodgingService: LodgingService) {}
 
   onSubmit(form: NgForm) {
-    console.log('Your form data : ', form.value);
-
     let occupancy = form.value.adults + form.value.children;
-
     let city = form.value.location;
     let checkIn = form.value['check-in'];
     let checkOut = form.value['check-out'];
@@ -46,43 +41,14 @@ export class SearchBarComponent implements OnInit {
   searchByAll(city: string, occupancy: number, checkIn: Date, checkOut: Date): void {
     this.lodgings$ = this.lodgingService.get();
     this.searchResults = [];
-
     this.lodgingRentals = [];
     this.bookingRentals = [];
 
     this.getBookingRentals();
     this.getLodgingRentals();
-
     this.getAvailableRentals(checkIn, checkOut);
+    this.searchByCityAndOccupancy(city, occupancy);
 
-    console.log(this.lodgingRentals);
-    console.log(this.bookingRentals);
-    console.log(this.availableRentals);
-
-    // This is the same as search by city and occupancy function down below, however this chcecks available rentals against the
-    // lodging rentals so if the ids match, we can add the lodging to the search results
-    this.lodgings$.subscribe((lodgings) =>
-      lodgings.forEach((lodging) =>
-        lodging.rentals.forEach((lodgingRental) => {
-          this.availableRentals.forEach((availableRental) => {
-            if (lodgingRental.id === availableRental.id) {
-              console.log(lodgingRental);
-              if (
-                lodging.location.address.city == city &&
-                availableRental.rentalUnit.occupancy >= occupancy
-              ) {
-                if (!this.searchResults.includes(lodging)) {
-                  this.searchResults.push(lodging);
-                  console.log(this.searchResults);
-                }
-              }
-            }
-          });
-        })
-      )
-    );
-
-    console.log(this.searchResults);
     this.lodgings$ = of(this.searchResults);
   }
 
@@ -112,17 +78,14 @@ export class SearchBarComponent implements OnInit {
         if (checkIn < booking.stay.checkIn && checkOut < booking.stay.checkIn) {
           this.lodgingRentals.forEach((lodgingRental) => {
             if (booking.rentals.includes(lodgingRental)) {
-              console.log(`lodging rental ${lodgingRental.name} is available`);
               this.availableRentals.push(lodgingRental);
             } else if (!booking.rentals.includes(lodgingRental)) {
-              console.log(`lodging rental ${lodgingRental.name} is available`);
               this.availableRentals.push(lodgingRental);
             }
           });
         } else if (checkIn > booking.stay.checkOut && checkOut > booking.stay.checkOut) {
           this.lodgingRentals.forEach((lodgingRental) => {
             if (booking.rentals.includes(lodgingRental)) {
-              console.log(`lodging rental ${lodgingRental.name} is available`);
               this.availableRentals.push(lodgingRental);
             }
           });
@@ -131,23 +94,26 @@ export class SearchBarComponent implements OnInit {
     );
   }
 
-  searchByCityAndOccupancy(city: string, occupancy: number) {
-    console.log(city, occupancy);
-    this.lodgings$ = this.lodgingService.get();
-    this.searchResults = [];
+  searchByCityAndOccupancy(city: string, occupancy: number): void {
     this.lodgings$.subscribe((lodgings) =>
-      lodgings.forEach((lodging) => {
-        lodging.rentals.forEach((rental) => {
-          if (rental.rentalUnit.occupancy >= occupancy && lodging.location.address.city == city) {
-            if (!this.searchResults.includes(lodging)) {
-              this.searchResults.push(lodging);
+      lodgings.forEach((lodging) =>
+        lodging.rentals.forEach((lodgingRental) => {
+          this.availableRentals.forEach((availableRental) => {
+            if (lodgingRental.id === availableRental.id) {
+              console.log(lodging);
+              if (
+                lodging.location.address.city === city &&
+                availableRental.rentalUnit.occupancy >= occupancy
+              ) {
+                if (!this.searchResults.includes(lodging)) {
+                  this.searchResults.push(lodging);
+                }
+              }
             }
-          }
-        });
-      })
+          });
+        })
+      )
     );
-
-    this.lodgings$ = of(this.searchResults);
   }
 
   ngOnInit(): void {
