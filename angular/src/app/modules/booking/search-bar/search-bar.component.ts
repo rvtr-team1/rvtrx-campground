f757ca77ceac405f2641d46c4f973b0e1a0c574b
Dusten Knull <dakatk97@gmail.com>
2020-08-13 14:44:59 -0500

    Cleaned up search-bar-component

--- angular/src/app/modules/booking/search-bar/search-bar.component.ts
+++ angular/src/app/modules/booking/search-bar/search-bar.component.ts
@@ -13,8 +13,6 @@ import { map } from 'rxjs/operators';
   templateUrl: './search-bar.component.html',
 })
 export class SearchBarComponent implements OnInit {
-  bookings$: Observable<Booking[]>;
-  lodgings$: Observable<Lodging[]>;
 
   @Output() searchResults = new EventEmitter<Lodging[]>();
   @Output() isSearched = new EventEmitter<boolean>();
@@ -24,10 +22,7 @@ export class SearchBarComponent implements OnInit {
     private readonly lodgingService: LodgingService
   ) {}
 
-  ngOnInit(): void {
-    this.lodgings$ = this.lodgingService.get();
-    this.bookings$ = this.bookingService.get();
-  }
+  ngOnInit(): void {}
 
   async onSubmit(form: NgForm) {
     const occupancy = form.value.adults + form.value.children;
@@ -36,84 +31,25 @@ export class SearchBarComponent implements OnInit {
     const checkIn = form.value['check-in'];
     const checkOut = form.value['check-out'];
 
-    await this.searchByAll(checkIn, checkOut, city, occupancy);
-    this.isSearched.emit(true);
-  }
+    this.lodgingService.getAvailable(city, occupancy).subscribe(
+      (lodgings) => {
 
-  private async getLodgingRentals(): Promise<Rental[]> {
-    return this.lodgings$
-      .pipe(
-        map<Lodging[], Rental[]>((lodgings) => {
-          const rentals = lodgings.map((lodging) => lodging.rentals);
-          return rentals.reduce((a, b) => a.concat(b));
-        })
-      )
-      .toPromise();
-  }
+        let bookedLodgingIds = []
 
-  private async getAvailableRentals(checkIn: Date, checkOut: Date): Promise<Rental[]> {
-    const lodgingRentals = await this.getLodgingRentals();
+        this.bookingService.getByDateRange(checkIn, checkOut).subscribe(
+          (bookings) => {
 
-    // Find rentals that don't already exist in
-    // bookings within the checkIn - checkOut range
-    return this.bookings$
-      .pipe(
-        map((bookings) => {
-          const availableRentals: Rental[] = [];
-
-          for (const booking of bookings) {
-            const bookingCheckIn = new Date(booking.stay.checkIn);
-            const bookingCheckOut = new Date(booking.stay.checkOut);
-
-            if (
-              (checkIn < bookingCheckIn && checkOut < bookingCheckIn) ||
-              (checkIn > bookingCheckOut && checkOut > bookingCheckOut)
-            ) {
-              for (const lodgingRental of lodgingRentals) {
-                if (!booking.rentals.includes(lodgingRental)) {
-                  availableRentals.push(lodgingRental);
-                }
-              }
+            for (const booking of bookings) {
+              bookedLodgingIds.push(booking.lodgingId);
             }
           }
+        );
+        
+        let availableLodgings = lodgings.filter((lodging) => !bookedLodgingIds.includes(lodging.id));
 
-          return availableRentals;
-        })
-      )
-      .toPromise();
-  }
-
-  async searchByAll(checkIn: Date, checkOut: Date, city: string, occupancy: number) {
-    const availableRentals: Rental[] = await this.getAvailableRentals(checkIn, checkOut);
-
-    function isAvailable(rental: Rental) {
-      for (const availableRental of availableRentals) {
-        if (rental.id === availableRental.id) {
-          return true;
-        }
+        this.searchResults.emit(availableLodgings);
+        this.isSearched.emit(true);
       }
-      return false;
-    }
-
-    // Most of this should be backend logic eventually,
-    // but this works for now
-    this.lodgings$.subscribe((lodgings) => {
-      const searchResults: Lodging[] = [];
-
-      for (const lodging of lodgings) {
-        if (lodging.location.address.city === city) {
-          const availableLodgingRentals = lodging.rentals.filter((rental) => isAvailable(rental));
-
-          for (const availableRental of availableLodgingRentals) {
-            if (availableRental.rentalUnit.occupancy >= occupancy) {
-              searchResults.push(lodging);
-              break;
-            }
-          }
-        }
-      }
-
-      this.searchResults.emit(searchResults);
-    });
+    )
   }
 }
