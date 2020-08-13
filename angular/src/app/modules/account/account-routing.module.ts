1e69adb472283889f37cfc6b8f673227d2baf7ab
Quentin Bullock <bullock.quentin@gmail.com>
2020-08-13 17:39:10 -0400

    removed unnecessary files

--- angular/src/app/modules/account/account-routing.module.ts
+++ angular/src/app/modules/account/account-routing.module.ts
@@ -8,7 +8,7 @@ import { PaymentComponent } from './payment/payment.component';
 import { ProfileComponent } from './profile/profile.component';
 import { AccountReviewComponent } from './account-review/account-review.component';
 import { EditableComponent } from './editable/editable.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AccountEditingService } from './services/account.editing.service';
 
 const routes: Routes = [{ component: AccountComponent, path: '' }];
@@ -25,6 +25,6 @@ const routes: Routes = [{ component: AccountComponent, path: '' }];
   ],
   providers: [AccountEditingService],
   exports: [RouterModule],
-  imports: [CommonModule, RouterModule.forChild(routes), FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
 })
 export class AccountRoutingModule {}
--- angular/src/app/modules/account/account.module.ts
+++ angular/src/app/modules/account/account.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { AccountRoutingModule } from './account-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AccountEditingService } from './services/account.editing.service';
 
 @NgModule({
-  imports: [AccountRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [AccountRoutingModule],
   providers: [AccountEditingService],
 })
 export class AccountModule {}
