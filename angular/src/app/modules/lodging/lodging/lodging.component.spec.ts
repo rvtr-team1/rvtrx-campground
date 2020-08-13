7114da040fb0fad47a008aec423077626fd94484
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-13 11:14:54 -0600

    fixing test spec files

--- angular/src/app/modules/lodging/lodging/lodging.component.spec.ts
+++ angular/src/app/modules/lodging/lodging/lodging.component.spec.ts
@@ -22,7 +22,6 @@ describe('LodgingComponent', () => {
           street: 'testStreet',
         },
         latitude: 'testLat',
-        locale: 'testLocale',
         longitude: 'testLong',
       },
       name: 'test',
