be67e608005d08bcdb77829ab0b894e068772d25
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-14 10:33:25 -0600

    refactored logic for rental component, updated documentation

--- angular/src/app/modules/lodging/rental/rental.component.ts
+++ angular/src/app/modules/lodging/rental/rental.component.ts
@@ -4,7 +4,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LodgingService } from '../../../services/lodging/lodging.service';
 import { Lodging } from '../../../data/lodging.model';
-import { Rental } from 'src/app/data/rental.model';
+import { Rental } from '../../../data/rental.model';
 
 /**
  * Rental component metadata
@@ -21,8 +21,8 @@ import { Rental } from 'src/app/data/rental.model';
 export class RentalComponent implements OnInit {
   /**
    * fields of the component
-   * rentalUnits: array of RentalUnits
-   * availabilityCount: maps number of available rentals to rentalUnit.id
+   * rentals: array of Rentals
+   * availabilityCount: maps number of available rentals to rental type
    */
   rentals: Rental[] = [];
   availabilityCount = new Map<string, number>();
@@ -39,32 +39,32 @@ export class RentalComponent implements OnInit {
 
   /**
    * uses a lodgingService to make a http get request to get
-   * lodging information then sends the lodgings to setRentalUnits method
+   * lodging information then sends the lodgings to setRentals method
    */
   private loadLodgings(): void {
     this.lodgingService.get().subscribe((lodgings) => {
-      this.setRentalUnits(lodgings);
+      this.setRentals(lodgings);
     });
   }
 
   /**
-   * populates rentalUnits array and keeps track of the availability of each rental
+   * populates rentals array and keeps track of the availability of each rental
    */
-  public setRentalUnits(lodgings: Lodging[]): void {
+  public setRentals(lodgings: Lodging[]): void {
     // get one lodging (hardcoded for now) from the lodging array
     // loop through its rentals
-    // check to see if a rental has duplicate rental units
-    // only keep track of the rental units that are unique
-    // increment the availability count for each rental unit if the rentals are available
+    // check to see if a rental has the same type as one that's already in the rentals array
+    // only keep track of the rental types that are unique
+    // increment the availability count for each rental in rentals if they are available
     lodgings[0].rentals.forEach((rental) => {
       if (!this.rentals.find((item) => item.type === rental.type)) {
         this.availabilityCount.set(rental.type, 0);
         this.rentals.push(rental);
-        if(rental.status === 'available'){
+        if (rental.status === 'available') {
           this.availabilityCount.set(rental.type, 1);
         }
       }
-      // The rental unit already exists in the array so just check availability and add it to the count
+      // The rental type already exists in the array so just check availability and add it to the count
       else {
         if (rental.status === 'available') {
           const count = this.availabilityCount.get(rental.type);
