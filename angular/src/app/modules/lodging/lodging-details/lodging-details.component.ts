62d3ad09a7a347710749842d95e5c037e1f6c197
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-14 15:53:35 -0600

    add readonly to dependencies

--- angular/src/app/modules/lodging/lodging-details/lodging-details.component.ts
+++ angular/src/app/modules/lodging/lodging-details/lodging-details.component.ts
@@ -20,7 +20,7 @@ export class LodgingDetailsComponent implements OnInit {
    * @param route route gives access to URL where the component will be displayed
    * @param lodgingService service that requests lodging information
    */
-  constructor(private route: ActivatedRoute, private lodgingService: LodgingService) {}
+  constructor(private readonly route: ActivatedRoute, private readonly lodgingService: LodgingService) {}
 
   /**
    * On init, get the lodging for which the details will be displayed
