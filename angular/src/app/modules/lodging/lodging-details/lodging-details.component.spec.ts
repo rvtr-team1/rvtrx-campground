15a23e3e37944b9b3e60c0c1288a1770bf334f87
Noah Funtanilla <noahfuntanilla6@gmail.com>
2020-08-14 13:50:10 -0600

    removed unused imports from lodging details test

--- angular/src/app/modules/lodging/lodging-details/lodging-details.component.spec.ts
+++ angular/src/app/modules/lodging/lodging-details/lodging-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LodgingDetailsComponent } from './lodging-details.component';
 import { Lodging } from 'src/app/data/lodging.model';
 import { of } from 'rxjs';
