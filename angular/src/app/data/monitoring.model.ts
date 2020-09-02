e9f7864e8a457b04d4500fed59809c2542911ca9
Nick Escalona <nicholas.escalona@gmail.com>
2020-09-02 01:26:43 -0500

    ng update

--- angular/src/app/data/monitoring.model.ts
+++ angular/src/app/data/monitoring.model.ts
@@ -1,5 +1,7 @@
 import * as Sentry from '@sentry/browser';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class Monitoring {
   sentry = {
     captureException: Sentry.captureException,
