eb7834c3bcd82351bb7c27668dfce82a1839bd3d
Harold Hopson <hdhopson88@yahoo.com>
2020-09-04 08:45:57 -0700

    removed unnecessary import of form module from account-routing

--- angular/src/app/modules/booking/booking-routing.module.ts
+++ angular/src/app/modules/booking/booking-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { BookingComponent } from './booking/booking.component';
 
 const routes: Routes = [{ component: BookingComponent, path: '' }];
 
 @NgModule({
   exports: [RouterModule],
-  imports: [FormsModule, RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes)],
 })
 export class BookingRoutingModule {}
