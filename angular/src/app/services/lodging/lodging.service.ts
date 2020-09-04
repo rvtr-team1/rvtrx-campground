97983aa032cfe2d82a06ce2c2aead4e1b5706d26
kyle guers <kguers@live.esu.edu>
2020-09-04 11:15:40 -0400

    changed lodging services delete return type to Response

--- angular/src/app/services/lodging/lodging.service.ts
+++ angular/src/app/services/lodging/lodging.service.ts
@@ -51,9 +51,9 @@ export class LodgingService {
    *
    * @param id string
    */
-  delete(id: string): Observable<boolean> {
+  delete(id: string): Observable<Response> {
     return this.lodgingsUrl$.pipe(
-      concatMap((url) => this.http.delete<boolean>(url, { params: { id } }))
+      concatMap((url) => this.http.delete<Response>(url, { params: { id } }))
     );
   }
 
