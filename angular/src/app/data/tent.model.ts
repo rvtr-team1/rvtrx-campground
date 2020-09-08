55c121f157caf8b043521351d93abbbd076d05c5
henry <kyeel123@gmail.com>
2020-09-08 10:39:10 -0400

    fixed the availability count issue

--- angular/src/app/data/tent.model.ts
+++ angular/src/app/data/tent.model.ts
@@ -6,10 +6,12 @@ export class Tent implements RentalProperties {
     size: plotSize;
     amenities: Amenities;
     maximumCapacity: number;
+    name:string;
     constructor(Size:plotSize)
     {
         this.size = Size;
         this.maximumCapacity = 4;
+        this.name = "tent";
     }
     
 
