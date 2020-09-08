e77a94e4a54ce2f5b5bdb17cb8ac399020d58a8e
Harold Hopson <hdhopson88@yahoo.com>
2020-09-08 09:12:49 -0700

    fixed rebase merge conflicts

--- angular/src/app/modules/account/account-routing.module.ts
+++ angular/src/app/modules/account/account-routing.module.ts
@@ -1,9 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { GenericEditingService } from 'services/editable/generic-editing.service';
 import { AccountComponent } from './account/account.component';
+<<<<<<< HEAD
 import { AccountBookingComponent } from './account-booking/account-booking.component';
 import { AddressComponent } from './address/address.component';
 import { PaymentComponent } from './payment/payment.component';
@@ -12,22 +12,13 @@ import { AccountReviewComponent } from './account-review/account-review.componen
 import { NewAddressFormComponent } from './new-address-form/new-address-form.component';
 import { NewPaymentFormComponent } from './new-payment-form/new-payment-form.component';
 import { EditableComponent } from './editable/editable.component';
+=======
+>>>>>>> updated account module w/declarations dict, form and common modules
 import { ACCOUNT_EDITING_SERVICE } from './account-editing.token';
 
 const routes: Routes = [{ component: AccountComponent, path: '' }];
 
 @NgModule({
-  declarations: [
-    AccountComponent,
-    AccountBookingComponent,
-    AddressComponent,
-    PaymentComponent,
-    ProfileComponent,
-    AccountReviewComponent,
-    NewAddressFormComponent,
-    NewPaymentFormComponent,
-    EditableComponent,
-  ],
   providers: [
     {
       provide: ACCOUNT_EDITING_SERVICE,
@@ -35,6 +26,6 @@ const routes: Routes = [{ component: AccountComponent, path: '' }];
     },
   ],
   exports: [RouterModule],
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [RouterModule.forChild(routes)],
 })
 export class AccountRoutingModule {}
--- angular/src/app/modules/account/account.module.ts
+++ angular/src/app/modules/account/account.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
 import { AccountRoutingModule } from 'modules/account/account-routing.module';
+=======
+import { CommonModule } from '@angular/common';
+import { AccountRoutingModule } from './account-routing.module';
+import { AccountComponent } from './account/account.component';
+import { AccountBookingComponent } from './account-booking/account-booking.component';
+import { AddressComponent } from './address/address.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AccountReviewComponent } from './account-review/account-review.component';
+import { NewAddressFormComponent } from './new-address-form/new-address-form.component';
+import { NewPaymentFormComponent } from './new-payment-form/new-payment-form.component';
+import { EditableComponent } from './editable/editable.component';
+import { ReactiveFormsModule } from '@angular/forms';
+>>>>>>> updated account module w/declarations dict, form and common modules
 
 @NgModule({
-  imports: [AccountRoutingModule],
+  declarations: [
+    AccountComponent,
+    AccountBookingComponent,
+    AddressComponent,
+    PaymentComponent,
+    ProfileComponent,
+    AccountReviewComponent,
+    NewAddressFormComponent,
+    NewPaymentFormComponent,
+    EditableComponent,
+  ],
+  imports: [CommonModule, AccountRoutingModule, ReactiveFormsModule],
 })
 export class AccountModule {}
