bef6018b25e2550aba05a99533129920cb47e326
henry <kyeel123@gmail.com>
2020-09-08 13:03:00 -0400

    fixed naming

--- angular/src/app/data/rv.model.ts
+++ angular/src/app/data/rv.model.ts
@@ -2,17 +2,13 @@ import { RentalProperties } from './rentalProperties.model';
 import { plotSize } from './plotSize.model';
 import { Amenities } from './amenities.model';
 
-export class RV implements RentalProperties {
-    size: plotSize;
-    amenities: Amenities;
-    maximumCapacity: number;
-    name:string;
+export class RVPlot implements RentalProperties {
     constructor(S:plotSize,A:Amenities)
     {
         this.size = S;
         this.amenities = A;
         this.maximumCapacity = 5;
-        this.name = "RV";
+        this.name = "RVPlot";
     }
     
 
--- angular/src/app/data/tent.model.ts
+++ angular/src/app/data/tent.model.ts
@@ -2,16 +2,12 @@ import { RentalProperties } from './rentalProperties.model';
 import { plotSize } from './plotSize.model';
 import { Amenities } from './amenities.model';
 
-export class Tent implements RentalProperties {
-    size: plotSize;
-    amenities: Amenities;
-    maximumCapacity: number;
-    name:string;
+export class TentPLot implements RentalProperties {
     constructor(Size:plotSize)
     {
         this.size = Size;
         this.maximumCapacity = 4;
-        this.name = "tent";
+        this.name = "TentPLot";
     }
     
 
