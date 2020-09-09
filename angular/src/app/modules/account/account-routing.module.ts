2cb6a8fbf8f1115d1f120c754f38c1f8a48a7246
Harold Hopson <hdhopson88@yahoo.com>
2020-09-08 23:52:16 -0700

    fixed merge conflict

--- angular/src/app/modules/account/account-routing.module.ts
+++ angular/src/app/modules/account/account-routing.module.ts
@@ -3,17 +3,6 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { GenericEditingService } from 'services/editable/generic-editing.service';
 import { AccountComponent } from './account/account.component';
-<<<<<<< HEAD
-import { AccountBookingComponent } from './account-booking/account-booking.component';
-import { AddressComponent } from './address/address.component';
-import { PaymentComponent } from './payment/payment.component';
-import { ProfileComponent } from './profile/profile.component';
-import { AccountReviewComponent } from './account-review/account-review.component';
-import { NewAddressFormComponent } from './new-address-form/new-address-form.component';
-import { NewPaymentFormComponent } from './new-payment-form/new-payment-form.component';
-import { EditableComponent } from './editable/editable.component';
-=======
->>>>>>> updated account module w/declarations dict, form and common modules
 import { ACCOUNT_EDITING_SERVICE } from './account-editing.token';
 
 const routes: Routes = [{ component: AccountComponent, path: '' }];
