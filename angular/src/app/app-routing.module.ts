7928090d911e1ea5c3fd613debd0823d89bd7813
fred belotte <fredbelotte@outlook.com>
2020-09-13 17:37:12 -0500

    fix dependency code smell issues [#174698357]

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
-import { ErrorComponent } from 'modules/home/error/error.component';
 
 const routes: Routes = [
   {
