50ecc5d9ed0b99693584b9a36f34dc419446fe1d
Ian Gilbert <ian.d.gilbert@gmail.com>
2020-09-14 13:48:07 -0700

    create addCard test in payment component

--- angular/src/app/modules/account/payment/payment.component.spec.ts
+++ angular/src/app/modules/account/payment/payment.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { PaymentComponent } from './payment.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable, of } from 'rxjs';
+import { PostPayment } from '../../../data/payment.model';
+import { AccountService } from '../../../services/account/account.service';
 
 describe('PaymentComponent', () => {
   const payments = [
@@ -12,6 +15,19 @@ describe('PaymentComponent', () => {
       securityCode: '',
     },
   ];
+  const mockCard: PostPayment = {
+    accountId: 'string',
+    id: 'string',
+    cardExpirationDate: '2020-08-01',
+    cardName: 'string',
+    cardNumber: 'string',
+    securityCode: '111',
+  };
+  const accountServiceStub = {
+    postPayment(card: PostPayment): Observable<PostPayment> {
+      return of(card);
+    },
+  };
   let component: PaymentComponent;
   let fixture: ComponentFixture<PaymentComponent>;
 
@@ -19,6 +35,7 @@ describe('PaymentComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         imports: [HttpClientTestingModule],
+        providers: [{ provide: AccountService, useValue: accountServiceStub }],
         declarations: [PaymentComponent],
       }).compileComponents();
     })
@@ -33,5 +50,10 @@ describe('PaymentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add new card', () => {
+    component.addCard(mockCard);
+    expect(component.payments.length).toEqual(2);
   });
 });
